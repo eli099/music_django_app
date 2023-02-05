21e35aeb74df4069047a7f1248fc1eab33f49f1b
Eliott McKenzie <e4991m@gmail.com>
2023-02-05 19:20:14 +0000

    defined Album model

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
