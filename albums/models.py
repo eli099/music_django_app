1c11cdcfe498e6496c652cc966c931677f95541a
Eliott McKenzie <e4991m@gmail.com>
2023-02-11 18:29:48 +0000

    defined how Album model shows in admin (returns a string with album details)

--- albums/models.py
+++ albums/models.py
@@ -5,3 +5,6 @@ class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f"{self.title} - {self.artist} ({self.year})"
